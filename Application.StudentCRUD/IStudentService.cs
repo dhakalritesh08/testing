4901a55774e83c28f44f67cccac07235f099472a
Ritesh Dhakal <dhakalritesh11@gmail.com>
2024-04-02 09:08:19 +0545

    Add project files.

---
+++ Application.StudentCRUD/IStudentService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.StudentCRUD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.StudentCRUD
+{
+    public interface IStudentService
+    {
+        Task<Student> AddStudent(Student student);
+        Task<IEnumerable<Student>> GetAllStudent();
+        Task<Student> UpdateStudent(Student student);
+        Task DeleteStudent(string id);
+        Task<IEnumerable<Student>> GetAllStudents(string id);
+    }
+}
