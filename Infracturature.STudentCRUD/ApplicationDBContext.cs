08ca6fc79132b73246d496325e6a81394c0fb22f
Ritesh Dhakal <dhakalritesh11@gmail.com>
2024-04-02 09:27:27 +0545

    fixes

---
+++ Infracturature.STudentCRUD/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.StudentCRUD;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infracturature.STudentCRUD
+{
+    public class ApplicationDBContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP - EJIH7BA; Database = NetApiTesting; Trusted_Connection = True; TrustServerCertificate = True; MultipleActiveResultSets = True;");
+        }
+        public DbSet<Student> Student { get; set; }
+       
+      
+    }
+}
